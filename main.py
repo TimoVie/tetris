adef99c439fc618ae5f5c029ccaa77d5807a5d8a
Gnomkrieger103 <48415209+Gnomkrieger103@users.noreply.github.com>
2023-12-17 18:34:24 +0100

    added Kommentare 3.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 import random
 from pygame import mixer
 
-pygame.font.init()
+pygame.font.init() #initialisiert das Schriftmodul in pygame
 
 # Globale Variablen
 lautstärke = 0
@@ -13,8 +13,8 @@ screen_höhe = screen_breite
 spiel_breite = screen_breite / 2
 spiel_höhe = screen_breite
 block_größe = spiel_höhe / 20
-oben_links_x = screen_breite - spiel_breite
-oben_links_y = screen_höhe - spiel_höhe
+oben_links_x = screen_breite - spiel_breite #x-Position der oberen linken Ecke des Spielfeldes
+oben_links_y = screen_höhe - spiel_höhe #y-Position der oberen linken Ecke des Spielfeldes
 
 MENÜ_eventtype = pygame.event.custom_type()
 PAUSE_eventtype = pygame.event.custom_type()
@@ -22,6 +22,8 @@ QUIT_eventtype = pygame.event.custom_type()
 
 
 # Tetrominos
+#Definition der Formen der Tetrominos
+
 O = [
   ['-', '-', '-', '-', '-'],
   ['-', '-', '-', '-', '-'],
@@ -79,8 +81,8 @@ T = [
 ]
 
 tetrominos = [O, I, S, Z, L, J, T]
-tetromino_farben = [(0, 110, 176), (255, 125, 0), (255, 201, 0), (48, 3, 156), (128, 207, 255), (255, 190, 128), (167, 130, 255)]
-schrift = pygame.font.Font('Montserrat-VariableFont_wght.ttf', int(screen_breite / 20))
+tetromino_farben = [(0, 110, 176), (255, 125, 0), (255, 201, 0), (48, 3, 156), (128, 207, 255), (255, 190, 128), (167, 130, 255)] #Definition der Farben der Tetrominos
+schrift = pygame.font.Font('Montserrat-VariableFont_wght.ttf', int(screen_breite / 20)) #Definition der Schriftart und -größe
 
 class Stück(object):
     def __init__(self, spalte, reihe, tetromino):
@@ -90,7 +92,7 @@ class Stück(object):
         self.x = spalte
         self.y = reihe
 
-def erstelle_grid(gesperrte_positionen={}):
+def erstelle_grid(gesperrte_positionen={}): #Erstellt ein Raster (Grid)
     grid = [[(30,30,30) for _ in range(10)] for _ in range(20)]
 
     for i in range(len(grid)):
@@ -100,7 +102,7 @@ def erstelle_grid(gesperrte_positionen={}):
                 grid[i][j] = c
     return grid
 
-def convertiere_tetromino_format(tetromino):
+def convertiere_tetromino_format(tetromino): #Konvertiert die From des Tetrominos in eine Liste von Positionen
     positionen = []
     interne_positionen = tetromino.shape
 
@@ -118,7 +120,7 @@ def convertiere_tetromino_format(tetromino):
 
     return positionen
 
-def platz_gültig(tetromino, grid):
+def platz_gültig(tetromino, grid): #Überprüft, ob der gegebene Tetromino an einer gültigen Position ist
     akzeptierte_positionen = [[(j, i) for j in range(10) if grid[i][j] == (30,30,30)] for i in range(20)]
     akzeptierte_positionen = [j for sub in akzeptierte_positionen for j in sub]
     formatiert = convertiere_tetromino_format(tetromino)
@@ -129,7 +131,7 @@ def platz_gültig(tetromino, grid):
                 return False
     return True
 
-def überprüfe_verloren(positionen):
+def überprüfe_verloren(positionen): #Überprüft, ob das Spiel verloren ist (ein Tetromino hat die oberste Reihe erreicht)
     for position in positionen:
         x, y = position
         if y < 1:
@@ -137,7 +139,7 @@ def überprüfe_verloren(positionen):
     return False
 
 
-def tetromino_erzeugen(aktuelles_tetromino = None):
+def tetromino_erzeugen(aktuelles_tetromino = None): #Gibt ein neues Tetromino zurück, das nicht die gleiche Form wie das aktuelle hat
     global tetrominos, tetromino_farben
     neues_stück = Stück(5, 0, random.choice(tetrominos))
     if aktuelles_tetromino is not None:
@@ -145,7 +147,7 @@ def tetromino_erzeugen(aktuelles_tetromino = None):
             neues_stück = Stück(5, 0, random.choice(tetrominos))
     return neues_stück
 
-def grid_zeichnen(oberfläche, anzahl_reihen, spalten):
+def grid_zeichnen(oberfläche, anzahl_reihen, spalten): #Zeichnet das Raster (Grid)
     start_x = oben_links_x
     start_y = oben_links_y
 
@@ -154,7 +156,7 @@ def grid_zeichnen(oberfläche, anzahl_reihen, spalten):
         for j in range(spalten):
             pygame.draw.line(oberfläche, (128,128,128), (start_x + j * block_größe, start_y), (start_x + j * block_größe, start_y + spiel_höhe))  # vertical lines
 
-def reihen_leeren(grid, gesperrt):
+def reihen_leeren(grid, gesperrt): #Entfernt alle vollen Reihen aus dem Raster und verschiebt die darüberliegenden Reihen nach unten
     zu_leerende_reihen = []
     for reihe_index in range(len(grid) - 1, -1, -1):
         reihe = grid[reihe_index]
@@ -176,7 +178,7 @@ def reihen_leeren(grid, gesperrt):
             gesperrt[neuer_Schlüssel] = gesperrt.pop(key)
     return len(zu_leerende_reihen)
 
-def nächstes_tetromino_zeichnen(tetromino, oberfläche):
+def nächstes_tetromino_zeichnen(tetromino, oberfläche): #Zeichnet das nächste Tetromino
     erster_index = 2
     letzter_index = 2
     for reihe_index, reihe in enumerate(tetromino.shape):
@@ -198,7 +200,7 @@ def nächstes_tetromino_zeichnen(tetromino, oberfläche):
             if spalte == 'X':
                 pygame.draw.rect(oberfläche, tetromino.farbe, (start_x + spalten_index * block_größe, start_y +  reihe_index * block_größe, block_größe, block_größe), 0)
 
-def spielfenster_erzeugen(oberfläche):
+def spielfenster_erzeugen(oberfläche): #Zeichnet das Fenster des Spiels
     oberfläche.fill((255,255,255))
 
     logo = pygame.image.load('logo_ba.jpg')
@@ -218,7 +220,7 @@ def spielfenster_erzeugen(oberfläche):
     grid_zeichnen(oberfläche, 20, 10)
     pygame.draw.rect(oberfläche, (189, 199, 192), (oben_links_x, oben_links_y, spiel_breite, spiel_höhe), int(spiel_breite / 120))
 
-def score_zeichnen(oberfläche, score):
+def score_zeichnen(oberfläche, score): #Zeichnet den aktuellen Score (Spielstand)
     schrift = pygame.font.Font('Montserrat-VariableFont_wght.ttf', int(screen_breite / 20))
     punktzahl_label = schrift.render(str(score), 1, (255, 255, 255))
 
@@ -342,8 +344,8 @@ def wait(key):
                 return
 
 def quit():
-    pygame.quit()
-    sys.exit()
+    pygame.quit() #Beendet alle PyGame-Module
+    sys.exit() #Beendet das Python-Skript
 
 def hauptmenü():
     global benutzer
@@ -388,12 +390,12 @@ def hauptmenü():
 
         benutzer = benutzername
 
-        pygame.display.update()
+        pygame.display.update() #Aktualisiert das Fenster
 
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            pygame.quit() #Beendet alle PyGame-Module
+            sys.exit() #Beendet das Python-Skript
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_BACKSPACE:
              benutzername = benutzername[:-1]
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
@@ -411,20 +413,20 @@ def pausenmenü(oberfläche):
 
         pygame.draw.rect(oberfläche, (30, 30, 30), (screen_breite - spiel_breite + spiel_breite / 240, screen_höhe - spiel_höhe + spiel_breite / 240, spiel_breite - 2 * spiel_breite / 240, spiel_höhe - 2 * spiel_breite / 240), 0)
         button("Fortsetzen", screen_breite - spiel_breite // 2 - spiel_breite // 4, screen_höhe // 2 - spiel_höhe // 40, spiel_breite // 2, spiel_höhe // 20, (0, 63, 115), (26, 99, 201), PAUSE_eventtype, int(spiel_breite // 20))
-        pygame.display.update()
+        pygame.display.update() #Aktualisiert das Fenster
 
         event = pygame.event.wait()
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            pygame.quit() #Beendet alle PyGame-Module
+            sys.exit() #Beendet das Python-Skript
         elif event.type == PAUSE_eventtype:
             pausiert = False
             print("Pause verlassen")
 
 
 def button(text, x, y, breite, höhe, inactive_farbe, active_farbe, aktion=None, schriftgröße = 20):
-    mausposition = pygame.mouse.get_pos()
-    mausklick = pygame.mouse.get_pressed()
+    mausposition = pygame.mouse.get_pos() #Position der Maus
+    mausklick = pygame.mouse.get_pressed() #Ob die Maustaste gedrückt wurde
 
     if x < mausposition[0] < x + breite and y < mausposition[1] < y + höhe:
         pygame.draw.rect(win, active_farbe, (x, y, breite, höhe))
@@ -443,19 +445,19 @@ def text_objekte(text, schrift):
     return text_oberfläche, text_oberfläche.get_rect()
 
 
-def score_speichern(benutzername, score):
+def score_speichern(benutzername, score): #Speichert den Score in einer Datei
     try:
-        scores = pickle.load(open('benutzer_score.pkl', 'rb'))
-    except (FileNotFoundError, EOFError):
+        scores = pickle.load(open('benutzer_score.pkl', 'rb')) #Versucht die gespeicherten Punktestände zu laden
+    except (FileNotFoundError, EOFError): #Wenn keine gefunden werden wird eine leere Liste erstellt
         scores = []
 
     scores.append((benutzername, score))
-    scores.sort(key=lambda x: x[1], reverse=True)  # Sort scores in descending order
+    scores.sort(key=lambda x: x[1], reverse=True)  #Sortiet die Liste in absteigender Reihenfolge
 
     with open('benutzer_score.pkl', 'wb') as datei:
         pickle.dump(scores, datei)
 
-def lade_top_scores():
+def lade_top_scores(): #Gibt die besten 3 Scores zurück
     try:
         scores = pickle.load(open('benutzer_score.pkl', 'rb'))
         return scores[:3]  # Return top 3 scores
@@ -466,8 +468,8 @@ def lade_top_scores():
 
 
 mixer.init()
-mixer.music.load('Soundtrack.mp3')
-mixer.music.set_volume(lautstärke)
+mixer.music.load('Soundtrack.mp3') #Läd die Hintergrundmusik
+mixer.music.set_volume(lautstärke) #Setzt die Lautstärke der Musik fest
 mixer.music.play(100)
 win = pygame.display.set_mode((screen_breite, screen_höhe))
 pygame.display.set_caption('Tetris Game')
